63740f25df6ef94d6e3ecac1f9fd1833b4d3c151
Itzik Dabush <itikdb@gmail.com>
2019-09-06 14:38:21 +0300

    init project

---
+++ src/Palette.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import './Palette.css';
+
+class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const colorBoxes = this.props.colors.map(color => (
+        <ColorBox background={color.color} name={color.name}/>
+    ));
+
+    return (
+      <div className="Palette">
+        {/* navbar goes here */}
+        <div className="Palette-colors">{colorBoxes}</div>
+            
+      </div>
+      // footer
+    );
+  }
+}
+
+export default Palette;
