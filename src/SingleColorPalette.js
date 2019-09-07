a812462d11502a2b1a04a6d68f4a695aa4f7453d
Itzik Dabush <itikdb@gmail.com>
2019-09-07 20:32:27 +0300

    adding link from the single color palettes

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
-import PaletteFooter from './PaletteFooter'
+import PaletteFooter from "./PaletteFooter";
+import { Link } from "react-router-dom";
 
 
 class SingleColorPalette extends Component {
   constructor(props) {
     super(props);
-      this._shades = this.gatherShades(this.props.palette, this.props.colorId);
-      this.state = {
-          format: 'hex'
-      }
-      this.changeFormat = this.changeFormat.bind(this);
+    this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+    this.state = {
+      format: "hex"
+    };
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   gatherShades(palette, colorToFilterBy) {
@@ -24,33 +25,34 @@ class SingleColorPalette extends Component {
       );
     }
     return shades.slice(1);
-    
+
     //return all shades of given color
   }
 
   changeFormat(value) {
     this.setState({ format: value });
   }
-    render() {
-      const { format } = this.state;
-      const { paletteName, emoji } = this.props.palette;
+  render() {
+    const { format } = this.state;
+    const { paletteName, emoji, id } = this.props.palette;
     const colorBoxes = this._shades.map(color => (
       <ColorBox
-        key={color.id}
+        key={color.name}
         name={color.name}
         background={color[format]}
         showLink={false}
       />
     ));
     return (
-        <div className="Palette">
-        <Navbar
-                handleChange={this.changeFormat}
-                showingAllColors={false}
-
-        />
-        <div className="Palette-colors">{colorBoxes}</div>
-        <PaletteFooter paletteName={paletteName} emoji={emoji}/>
+      <div className="SingleColorPalette Palette">
+        <Navbar handleChange={this.changeFormat} showingAllColors={false} />
+        <div className="Palette-colors">
+          {colorBoxes}
+          <div className='go-back ColorBox'>
+            <Link to={`/palette/${id}`} className='back-button'>GO BACK</Link>
+        </div>
+        </div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
