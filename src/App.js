dfbc7b08e1346d2cb39690647f1aed70433e1e84
Itzik Dabush <itikdb@gmail.com>
2019-09-16 15:05:18 +0300

    save paletes to localStorage

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ import NewPaletteForm from './NewPaletteForm'
 
 class App extends Component {
   constructor(props) {
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
+
     super(props);
-    this.state = {palettes: seedColors}
+    this.state = {palettes: savedPalettes || seedColors}
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
   }
@@ -22,7 +24,13 @@ class App extends Component {
   }
 
   savePalette(newPalette) {
-    this.setState({palettes: [...this.state.palettes, newPalette]})
+    this.setState({ palettes: [...this.state.palettes, newPalette] },  this.syncLocalStorage);
+   
+    
+  }
+
+  syncLocalStorage() {
+    window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes))
   }
   render() {
     return (
